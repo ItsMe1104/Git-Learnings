164bd83483e368d7d3af021f923589de1bf0625b
Hrithik <hey.its.me.hrithik@gmail.com>
2024-11-26 10:16:02 +0530

    Add initial files for chapter 4

---
+++ 04. Create and Merge Branches/notes.js
@@ -0,0 +1,29 @@
+// 1) Story of Branches in Git :-
+
+// --> The main Branch in Git is called the "master" branch. In Github its called the "main" branch.
+// --> The final code in this branch is actually used to publish our live site
+// --> Hence, all the team members are not allowed to directly contribute to the master branch as any bug or issue might reflect on the live website.
+// --> Hence, for creating a new feature, we always create a new branch.
+// --> The new branch will have all the code, till the current breakpoint.
+// --> After adding code in this new branch and testing it, we try to merge it with our master branch
+// --> For merging in github, we have to create a pull request and the team leads might merge it.
+
+
+
+
+//********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************* */
+
+
+
+// 2) Git branches :-
+
+
+// a) To see all the branches in our respository :-
+// --> Use "git branch"                (The star symbol will specify in which branch we currently are)
+
+
+// b) To create a new branch :-
+// --> Use "git branch branch_name"    (The branch emerges from the current working branch )
+
+
+// c) 
