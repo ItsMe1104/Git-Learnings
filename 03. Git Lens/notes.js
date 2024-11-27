b79e26cee5b0e9c0692c764ae8faf1fad7bbad19
Hrithik <hey.its.me.hrithik@gmail.com>
2024-11-27 23:38:45 +0530

    Modify notes file for Chapter 3

--- 03. Git Lens/notes.js
+++ 03. Git Lens/notes.js
@@ -3,3 +3,18 @@
 
 // Install the extension "gitlens" from extensions list in VS Code.
 // Try to learn the Git lens although its not compulsary
+
+
+
+
+//**************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************** */
+
+
+// 2) Git Graph Extension :-
+
+// --> Git Lens might get paid in the future hence, to see the graphs properly, we can use some other extension called "Git Graph" ny mhutchie
+// --> It only shows the graph
+
+// ==> After Installing, we can get a button in the bottom left of VS Code named "Git Graph"
+
+// ==> Click on that button to see our Git graph
